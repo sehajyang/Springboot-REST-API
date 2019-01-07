11ef5f031fc57ca8d7fd83385d43c88e49ecd4e3
yunjey0 <sehajyang@gmail.com>
2019-01-07 22:36:35 +0900

    fixed untracked files

--- src/main/java/me/sehajyang/io/events/EventController.java
+++ src/main/java/me/sehajyang/io/events/EventController.java
@@ -1,22 +1,27 @@
 package me.sehajyang.io.events;
 
-import java.net.URI;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.net.URI;
+
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping(value= "/api/events", produces = MediaTypes.HAL_JSON_UTF8_VALUE)
 public class EventController {
     
-    @PostMapping("/api/events")
+    @PostMapping
      public ResponseEntity createEvent(@RequestBody Event event) {
-          URI createdUri = linkTo(methodOn(EventController.class).createEvent(null)).slash("{id}").toUri(); 
+          URI createdUri = linkTo(EventController.class).slash("{id}").toUri(); 
            //hateos가 제공, Location URL만들기
-           return ResponseEntity.created(createdUri).build();
+          event.setId(10);
+           return ResponseEntity.created(createdUri).body(event);
            //201 응답 만듦
          
      }
