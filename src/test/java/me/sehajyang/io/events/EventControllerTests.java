0fd4a8bb5bb1b8c46c481fb852013d682e8aa790
yunjey0 <sehajyang@gmail.com>
2019-02-20 02:36:13 +0900

    Add EventControllerTest Bad_Request Test Case

--- src/test/java/me/sehajyang/io/events/EventControllerTests.java
+++ src/test/java/me/sehajyang/io/events/EventControllerTests.java
@@ -32,11 +32,39 @@ public class EventControllerTests {
     @Autowired
     ObjectMapper objectMapper;
 
-    @Autowired
-    EventRepository eventRepository;
-    
     @Test
     public void createEvent() throws Exception {
+        //요청 생성
+        Event event = Event.builder()
+                .name("Spring")
+                .description("Rest Api Dev")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019, 01, 04, 14, 20))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 01, 05, 14, 20))
+                .beginEventDateTime(LocalDateTime.of(2019, 01, 06, 14, 20))
+                .endEventDateTime(LocalDateTime.of(2019, 01, 07, 14, 20))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("Seoul, Korea")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                    .contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print()) // 어떠한 응답과 요청이 갔는지 볼 수 있음
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
+                ;
+    
+    }
+    @Test
+    public void createEvent_Bad_Request() throws Exception {
         //요청 생성
         Event event = Event.builder()
                 .id(100)
@@ -54,8 +82,7 @@ public class EventControllerTests {
                 .offline(false)
                 .eventStatus(EventStatus.PUBLISHED)
                 .build();
-        Mockito.when(eventRepository.save(event)).thenReturn(event);
-        
+
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
                     .accept(MediaTypes.HAL_JSON)
@@ -69,7 +96,7 @@ public class EventControllerTests {
                 .andExpect(jsonPath("free").value(Matchers.not(true)))
                 .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.name()))
                 ;
-    
+
     }
 
 }
